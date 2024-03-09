516da0181eccbc8009c00b26a71eb454f612873f
Bikash Bhandari <bhandari@johnsnowlabs.com>
2024-03-09 22:05:36 +0545

    update variable

--- terraform-provisioning/variables.tf
+++ terraform-provisioning/variables.tf
@@ -1,18 +1,22 @@
 variable "aws_region" {
   description = "AWS region"
+  default     = "us-east-1"  // Update with your desired AWS region
 }
 
 variable "cluster_version" {
   description = "EKS cluster version"
+  default     = "1.27"  // Update with your desired EKS cluster version
 }
 
 variable "subnets" {
   description = "Subnet IDs for EKS"
   type        = list(string)
+  default     = ["subnet-12345678", "subnet-87654321"]  // Update with your subnet IDs
 }
 
 variable "vpc_id" {
   description = "VPC ID for EKS"
+  default     = "vpc-12345678"  // Update with your VPC ID
 }
 
 variable "node_groups" {
@@ -24,4 +28,13 @@ variable "node_groups" {
     instance_type    = string
     key_name         = string
   }))
+  default = [
+    {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+      key_name         = "your-keypair-name"  // Update with your key pair name
+    }
+  ]
 }
